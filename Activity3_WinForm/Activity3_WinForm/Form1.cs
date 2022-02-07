8226213214bca6e1105dbc6a3ca896f9c35eada2
jhoneric <jhonerix23@gmail.com>
2022-02-07 08:11:33 -0500

    first commit

---
+++ Activity3_WinForm/Activity3_WinForm/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity3_WinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            if (rbtnOk.Checked == true)
+            {
+                // FOR OK
+                MessageBox.Show("This is your customized Ok Message Box", "Customized Ok Message Box", MessageBoxButtons.OK);   
+            }
+            if (rbtnOkCancel.Checked == true)
+            {
+                // FOR OK CANCEL
+                MessageBox.Show("This is your customized OkCancel Message Box", "Customized OkCancel Message Box", MessageBoxButtons.OKCancel);   
+            }
+            if (rbtnAbortRetryIgnore.Checked == true)
+            {
+                // FOR ABORT RETRY IGNORE
+                MessageBox.Show("This is your customized AbortRetryIgnore Message Box", "Customized AbortRetryIgnore Message Box", MessageBoxButtons.AbortRetryIgnore);   
+            }
+            if (rbtnYesNoCancel.Checked == true)
+            {
+                // FOR YES NO CANCEL
+                MessageBox.Show("This is your customized YesNoCancel Message Box", "Customized YesNoCancel Message Box", MessageBoxButtons.YesNoCancel);   
+            }
+            if (rbtnYesNo.Checked == true)
+            {
+                // FOR YES NO
+                MessageBox.Show("This is your customized YesNo Message Box", "Customized YesNo Message Box", MessageBoxButtons.YesNo);   
+            }
+            if (rbtnRetryCancel.Checked == true)
+            {
+                // FOR RETRY CANCEL
+                MessageBox.Show("This is your customized RetryCancel Message Box", "Customized RetryCancel Message Box", MessageBoxButtons.RetryCancel);   
+            }
+        }
+    }
+}
---
+++ Activity3_WinForm/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Activity3_WinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rbtnOk = new System.Windows.Forms.RadioButton();
+            this.rbtnOkCancel = new System.Windows.Forms.RadioButton();
+            this.rbtnAbortRetryIgnore = new System.Windows.Forms.RadioButton();
+            this.rbtnYesNoCancel = new System.Windows.Forms.RadioButton();
+            this.rbtnYesNo = new System.Windows.Forms.RadioButton();
+            this.rbtnRetryCancel = new System.Windows.Forms.RadioButton();
+            this.lblButtonType = new System.Windows.Forms.Label();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rbtnOk
+            // 
+            this.rbtnOk.AutoSize = true;
+            this.rbtnOk.BackColor = System.Drawing.SystemColors.Window;
+            this.rbtnOk.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rbtnOk.Font = new System.Drawing.Font("MS Reference Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtnOk.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.rbtnOk.Location = new System.Drawing.Point(441, 167);
+            this.rbtnOk.Name = "rbtnOk";
+            this.rbtnOk.Size = new System.Drawing.Size(68, 33);
+            this.rbtnOk.TabIndex = 0;
+            this.rbtnOk.TabStop = true;
+            this.rbtnOk.Text = "Ok";
+            this.rbtnOk.UseVisualStyleBackColor = false;
+            // 
+            // rbtnOkCancel
+            // 
+            this.rbtnOkCancel.AutoSize = true;
+            this.rbtnOkCancel.BackColor = System.Drawing.SystemColors.Window;
+            this.rbtnOkCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rbtnOkCancel.Font = new System.Drawing.Font("MS Reference Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtnOkCancel.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.rbtnOkCancel.Location = new System.Drawing.Point(441, 224);
+            this.rbtnOkCancel.Name = "rbtnOkCancel";
+            this.rbtnOkCancel.Size = new System.Drawing.Size(153, 33);
+            this.rbtnOkCancel.TabIndex = 1;
+            this.rbtnOkCancel.TabStop = true;
+            this.rbtnOkCancel.Text = "OkCancel";
+            this.rbtnOkCancel.UseVisualStyleBackColor = false;
+            // 
+            // rbtnAbortRetryIgnore
+            // 
+            this.rbtnAbortRetryIgnore.AutoSize = true;
+            this.rbtnAbortRetryIgnore.BackColor = System.Drawing.SystemColors.Window;
+            this.rbtnAbortRetryIgnore.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rbtnAbortRetryIgnore.Font = new System.Drawing.Font("MS Reference Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtnAbortRetryIgnore.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.rbtnAbortRetryIgnore.Location = new System.Drawing.Point(441, 275);
+            this.rbtnAbortRetryIgnore.Name = "rbtnAbortRetryIgnore";
+            this.rbtnAbortRetryIgnore.Size = new System.Drawing.Size(257, 33);
+            this.rbtnAbortRetryIgnore.TabIndex = 2;
+            this.rbtnAbortRetryIgnore.TabStop = true;
+            this.rbtnAbortRetryIgnore.Text = "AbortRetryIgnore";
+            this.rbtnAbortRetryIgnore.UseVisualStyleBackColor = false;
+            // 
+            // rbtnYesNoCancel
+            // 
+            this.rbtnYesNoCancel.AutoSize = true;
+            this.rbtnYesNoCancel.BackColor = System.Drawing.SystemColors.Window;
+            this.rbtnYesNoCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rbtnYesNoCancel.Font = new System.Drawing.Font("MS Reference Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtnYesNoCancel.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.rbtnYesNoCancel.Location = new System.Drawing.Point(441, 331);
+            this.rbtnYesNoCancel.Name = "rbtnYesNoCancel";
+            this.rbtnYesNoCancel.Size = new System.Drawing.Size(198, 33);
+            this.rbtnYesNoCancel.TabIndex = 3;
+            this.rbtnYesNoCancel.TabStop = true;
+            this.rbtnYesNoCancel.Text = "YesNoCancel";
+            this.rbtnYesNoCancel.UseVisualStyleBackColor = false;
+            // 
+            // rbtnYesNo
+            // 
+            this.rbtnYesNo.AutoSize = true;
+            this.rbtnYesNo.BackColor = System.Drawing.SystemColors.Window;
+            this.rbtnYesNo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rbtnYesNo.Font = new System.Drawing.Font("MS Reference Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtnYesNo.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.rbtnYesNo.Location = new System.Drawing.Point(441, 386);
+            this.rbtnYesNo.Name = "rbtnYesNo";
+            this.rbtnYesNo.Size = new System.Drawing.Size(113, 33);
+            this.rbtnYesNo.TabIndex = 4;
+            this.rbtnYesNo.TabStop = true;
+            this.rbtnYesNo.Text = "YesNo";
+            this.rbtnYesNo.UseVisualStyleBackColor = false;
+            // 
+            // rbtnRetryCancel
+            // 
+            this.rbtnRetryCancel.AutoSize = true;
+            this.rbtnRetryCancel.BackColor = System.Drawing.SystemColors.Window;
+            this.rbtnRetryCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rbtnRetryCancel.Font = new System.Drawing.Font("MS Reference Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtnRetryCancel.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.rbtnRetryCancel.Location = new System.Drawing.Point(441, 441);
+            this.rbtnRetryCancel.Name = "rbtnRetryCancel";
+            this.rbtnRetryCancel.Size = new System.Drawing.Size(187, 33);
+            this.rbtnRetryCancel.TabIndex = 5;
+            this.rbtnRetryCancel.TabStop = true;
+            this.rbtnRetryCancel.Text = "RetryCancel";
+            this.rbtnRetryCancel.UseVisualStyleBackColor = false;
+            // 
+            // lblButtonType
+            // 
+            this.lblButtonType.AutoSize = true;
+            this.lblButtonType.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.lblButtonType.Font = new System.Drawing.Font("MS Reference Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblButtonType.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.lblButtonType.Location = new System.Drawing.Point(403, 76);
+            this.lblButtonType.Name = "lblButtonType";
+            this.lblButtonType.Size = new System.Drawing.Size(295, 46);
+            this.lblButtonType.TabIndex = 6;
+            this.lblButtonType.Text = "BUTTON TYPE";
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.btnDisplay.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDisplay.Font = new System.Drawing.Font("MS Reference Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDisplay.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnDisplay.Location = new System.Drawing.Point(411, 542);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(287, 67);
+            this.btnDisplay.TabIndex = 7;
+            this.btnDisplay.Text = "DISPLAY";
+            this.btnDisplay.UseVisualStyleBackColor = false;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(1111, 757);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.lblButtonType);
+            this.Controls.Add(this.rbtnRetryCancel);
+            this.Controls.Add(this.rbtnYesNo);
+            this.Controls.Add(this.rbtnYesNoCancel);
+            this.Controls.Add(this.rbtnAbortRetryIgnore);
+            this.Controls.Add(this.rbtnOkCancel);
+            this.Controls.Add(this.rbtnOk);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rbtnOk;
+        private System.Windows.Forms.RadioButton rbtnOkCancel;
+        private System.Windows.Forms.RadioButton rbtnAbortRetryIgnore;
+        private System.Windows.Forms.RadioButton rbtnYesNoCancel;
+        private System.Windows.Forms.RadioButton rbtnYesNo;
+        private System.Windows.Forms.RadioButton rbtnRetryCancel;
+        private System.Windows.Forms.Label lblButtonType;
+        private System.Windows.Forms.Button btnDisplay;
+    }
+}
+
